423a3d09de644cd5edd23a48a753fc41295a6f78
Patrick <w.patrick.kelly@gmail.com>
2020-06-12 18:07:06 -0700

    Files transposed now, but I would like to make it modular

--- makesite.go
+++ makesite.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"os"
 	"flag"
+	"strings"
 
 )
 
@@ -15,18 +16,12 @@ type Content struct {
 	// Setting content structure as a string
 	Content string
 }
+
+```What is the right way to make the functions modular? I am not sure how to call
+these functions when the main() does not accept input or return output ```
 // Create main
 func main() {
-
-	file := flag.String("file", ".txt", "This flag represents the name of any `.txt` file in the same directory as your program.")
-	flag.Parse()
-	fmt.Println(*file)
-	// output := flag.Bool("output", false, "Should there be output?")
-	// input := flag.String("input", "file.csv", "the pather to the input file")
-	// flag.Parse()
-	// fmt.Println(*output)
-	// fmt.Println(*input)
-
+	// READ
 	// """Reading file content"""
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	// if there is an error, panic
@@ -37,6 +32,19 @@ func main() {
 	// print fileContent
 	fmt.Print(string(fileContents))
 
+
+	// TRANSPOSE
+	file := flag.String("file", "post.html", "This flag represents the name of any `.txt` file in the same directory as your program.")
+	flag.Parse()
+	// Print file to the console
+	fmt.Println(*file)
+	// set extention to a .html
+	extension := ".html"
+	// split from "." and replate with extention
+	newFile := strings.Split(*file, ".") [0] + extension
+
+
+	// RENDER
 	// set file path for ParseFile
 	path := []string{
 		"template.tmpl",
@@ -46,83 +54,9 @@ func main() {
 	// Make the new template from parsed path
 	t := template.Must(template.New("template.tmpl").ParseFiles(path...))
 	// Create html
-	create, _ := os.Create(*file)
+	create, _ := os.Create(newFile)
 	err2 := t.Execute(create, content)
 	if err2 != nil{
 		panic(err2)
 		}
-	}
-
-
-
-	// fileContents, err := ioutil.ReadFile("first-post.txt")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Print(string(fileContents))
-	//
-	// t := template.Must(template.New("template.tmpl").ParseFiles("first-post.txt"))
-	// err2 := t.Execute(os.Stdout, content)
-	// if err2 != nil {
-	// 	panic(err)
-	// }
-
-
-
-
-
-// func render(){
-//
-// // ToDo: Look up arguments for template.New function in documentation
-// 	t := template.Must(template.New("template.tmpl").ParseFiles("first-post.txt"))
-// 	err2 := t.Execute(os.Stdout, content)
-// 	if err2 != nil {
-// 		panic(err)
-// 	}
-// }
-
-
-
-// func readFile() string {
-// 	fileContents, err := ioutil.ReadFile("first-post.txt")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	return string(fileContents)
-// }
-
-
-
-
-
-
-
-// func temp_write(){
-// 	paths := []string{
-// 		"template.tmpl",
-// 	}
-// 	t := template.Must(template.New("html-tmpl").ParseFiles(paths...))
-// 	err := t.Execute(os.Stdout, template)
-// 	if err != nil {
-// 		panic(err)
-// 		}
-// }
-
-
-// func main() {
-// 	bytesToWrite := []byte("hello\ngo\n")
-// 	err := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-// func renderTemplate()
-// =======
-// import "fmt"
-//
-// func main() {
-// 	fmt.Println("Hello, world!")
-// }
-// >>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
+}
